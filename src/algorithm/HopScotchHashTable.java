09854888fe81df4d2824c4256bbd2df053803582
DESKTOP-T9L4TA9\fandong <1452125537@qq.com>
2019-12-30 18:09:09 +0800

    s

--- src/algorithm/HopScotchHashTable.java
+++ src/algorithm/HopScotchHashTable.java
@@ -6,6 +6,12 @@ import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * 跳房子散列
+ * 线性开放定址的扩展，规定一个MAX_DIST hash处的值可以存放在 hash 到 hash + MAX_DIST -1 的位置上
+ * 由此得到最坏情况下的查找时间为 O(MAX_DIST)
+ * @param <T>
+ */
 @Data
 public class HopScotchHashTable<T> {
 
@@ -13,6 +19,7 @@ public class HopScotchHashTable<T> {
     private static final double DEFAULT_LOAD_FACTOR = 0.8;
     private static final int MAX_DIST = 5;
 
+    private int currentSize;
     private Node<T>[] dataArray;
 
     @SuppressWarnings("all")
@@ -28,6 +35,80 @@ public class HopScotchHashTable<T> {
         private int hop;
     }
 
+    public boolean contains(T t){
+        return findPos(t) != -1;
+    }
+
+    public void remove(T t){
+        int pos = findPos(t);
+        if (pos > 0){
+            dataArray[pos] = null;
+            int index = myHash(t);
+            //偏移存储的情况下需要维护 原有位置的 hop值
+            if (index != pos){
+                dataArray[index].setHop(setBinaryIndexValue(dataArray[index].getHop(), pos - index, 0));
+            }
+            currentSize--;
+        }
+    }
+
+    public void insert(T t){
+        if (contains(t)){
+            return;
+        }
+        if (currentSize > DEFAULT_CAPACITY * DEFAULT_LOAD_FACTOR){
+            rehash();
+        }
+
+        int index = myHash(t);
+        for (int i = 0; i < MAX_DIST; i++) {
+            int currentIndex = index + i;
+            Node<T> tNode = dataArray[currentIndex];
+            if (tNode == null){
+
+            }
+        }
+    }
+
+    private void rehash(){
+        Node<T>[] oldArray = this.dataArray;
+        dataArray = (Node<T>[])new Object[Prime.nextPrime(oldArray.length * 2)];
+        for (Node<T> tNode : oldArray){
+            if (tNode != null){
+                insert(tNode.data);
+            }
+        }
+    }
+
+
+    private int getHop(int hash){
+        int hop = 0;
+        for (int i = 0; i < MAX_DIST; i++) {
+            Node<T> tNode = dataArray[hash + i];
+            if (tNode != null){
+                if (myHash(tNode.data) == hash){
+                    hop = setBinaryIndexValue(hop, i, 1);
+                }
+            }
+        }
+        return hop;
+    }
+
+    private int findPos(T t){
+        int index = myHash(t);
+        Node<T> tNode = dataArray[index];
+        if (tNode != null){
+            List<Integer> binaryToList = binaryToList(tNode.getHop());
+            for (Integer i : binaryToList){
+                int cur = i + index;
+                if (dataArray[cur].equals(t)){
+                    return cur;
+                }
+            }
+        }
+        return -1;
+    }
+
     private int myHash(T t){
         int h;
         int hash = t == null ? 0 : (h = t.hashCode()) ^ (h >>> 16);
@@ -37,6 +118,7 @@ public class HopScotchHashTable<T> {
         }
         return hash;
     }
+
 
     /**
      * 找到二进制位置上为 1 的索引
@@ -70,7 +152,7 @@ public class HopScotchHashTable<T> {
         }else if (value == 1 && !contains){
             return hop + pow;
         }
-        throw new IllegalArgumentException();
+        return hop;
     }
 
 }
